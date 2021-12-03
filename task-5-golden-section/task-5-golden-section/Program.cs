8e27e2b221bd7aeeed0edacac3dc26da0264b0d4
Petrovytsu <95479338+P3trow@users.noreply.github.com>
2021-12-03 15:38:15 +0300

    Add files via upload

---
+++ task-5-golden-section/task-5-golden-section/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace task_5_golden_section
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            const double FI = 1.618;
+            const double E = 0.000001;
+
+            const double A = -2;
+            const double B = 2;
+
+            double a = A;
+            double b = B;
+
+            while (b - a > E) {
+                double c = a + (b - a) / FI;
+                double x = a + b - c;
+
+                double fa = f(a); // не используется, но требуется посчитать
+                double fb = f(b); // не используется, но требуется посчитать
+                double fc = f(c);
+                double fx = f(x);
+
+                Console.WriteLine("a: " + a.ToString() + ", b: " + b.ToString() + ", x: " + x.ToString() + ", f(x)" + fx);
+
+                if (c > x) {
+                    if (fx < fc) {
+                        // a = a;
+                        b = c;
+                    } else {
+                        a = x;
+                        // b = b;
+                    }
+                } else {
+                    if (c < fc) {
+                        a = c;
+                        // b = b;
+                    } else {
+                        // a = a;
+                        b = x;
+                    }
+                }
+            }
+        }
+
+        static double f (double x) {
+            const double A = 1;
+            const double B = -1;
+            return A * Math.Pow(x, 2) + B * x;
+        }
+    }
+}
